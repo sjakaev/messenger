b48a824da2535f983fcaa7883a0857f1481c2625
Роман Сякаев <r.sjakaev@reg.ru>
2024-01-09 11:43:26 +0400

    adds express.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.static(path.join(process.cwd(), 'dist')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'dist', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'dist', 'src', 'pages', 'login', 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
+});
